f4ef10ce5e603a4f958a505b03fde5a064c3e3cd
whtuDT <whtuipe@gmail.com>
2019-05-16 19:14:48 +0800

    check and modified models.py orm.py ../schema.sql test.py files

--- www/orm.py
+++ www/orm.py
@@ -71,7 +71,7 @@ class Field(object):
 		self.default = default
 	
 	def __str__(self):
-		return '<%s, %s:%s>' % (self._class__.__name__, self.column_type, self.name)
+		return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 	
 class StringField(Field):
 	def __init__(self, name = None, primary_key = False, default = None, ddl = 'varchar(100)'):
@@ -87,7 +87,7 @@ class IntegerField(Field):
 
 class FloatField(Field):
 	def __init__(self, name = None, primary_key = False, default = 0.0):
-		super().__name__(name, 'real', primary_key, default)
+		super().__init__(name, 'real', primary_key, default)
 		
 class TextField(Field):
 	def __init__(self, name = None, default = None):
@@ -127,7 +127,7 @@ class ModelMetaclass(type):
 		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ','.join(escaped_fields), primaryKey,create_args_sting(len(escaped_fields)+1))
 		attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ','.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
 		attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
-		return type.__name__(cls, name, bases, attrs)
+		return type.__new__(cls, name, bases, attrs)
 
 class Model(dict, metaclass = ModelMetaclass):
 	def __init__(self, **kw):
---
+++ www/test.py
@@ -0,0 +1,14 @@
+import orm
+import asyncio
+from models import User, Blog, Comment
+
+async def test(loop):
+    await orm.create_pool(loop, user='www-data', password='www-data', db='awesome')
+
+    u = User(name='Test', email='test.example.com', password='1234567890', image ='about:blank')
+
+    await u.save()
+
+loop=asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
