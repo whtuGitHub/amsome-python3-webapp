d1f6e8584592df0b2cd678c324f57f80cdd68842
whtuDT <whtuipe@gmail.com>
2019-05-17 15:13:18 +0800

    add config_default.py

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'whtuDT'
+
+configs = {
+        'debug': True,
+        'db': {
+            'host': '127.0.0.1',
+            'port': 3306,
+            'user': 'data-www',
+            'password': 'data-www',
+            'db': 'awesome'
+            },
+        'session': {
+            'secret': 'Aweaome'
+            }
+        }
