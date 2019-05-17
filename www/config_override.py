d9147157c492e35c8eb3edcdd15412bc662fea8b
whtuDT <whtuipe@gmail.com>
2019-05-17 15:17:02 +0800

    add config_override.py

---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'whtuDT'
+
+configs = {
+        'db': {
+            'host': '192.168.0.100'
+            }
+        }
