d9f0ef989991d7e78fb72395b41ef39b3ad1e3a2
whtuDT <whtuipe@gmail.com>
2019-05-23 07:49:39 +0800

    add @get('/')

--- www/handlers.py
+++ www/handlers.py
@@ -5,3 +5,14 @@ __author__ = 'whtuDT'
 
 ' url handlers '
 
+from models import User
+from coroweb import get
+import asyncio
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+            '__template__': 'test.html',
+            'users': users
+    }
